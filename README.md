f660c0f797dbe46caa429a4e5fc34dda3f0dbe45
Mario Nevado <nevado.mario@gmail.com>
2021-05-07 13:23:58 +0200

    first cr build created

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ibm-cr-build
+# CR BUILD Action IBMCloud
+
+This action is to build an image into IBM Cloud
+
+## Entradas
+
+### `url`
+
+**Mandatory** url of the server like: de.icr.io
+
+### `namespace`
+
+**Mandatory** namspace to build the image
+
+### `imagename`
+
+**Mandatory** image name
+
+### `buildnr`
+
+**Mandatory** build nr: typically commit nr
+
+### `dir`
+
+**Mandatory** dir to find Dockerfile into the repository
+
+
+## Ejemplo de uso
+
+
+```yaml
+# File: .github/workflows/workflow.yml
+on: [push]
+
+name: example
+
+jobs:
+
+  devflow:
+    runs-on: ubuntu-latest
+    steps:
+    
+    - uses: mnevadom/ibm-cr-build@v1
+      with:
+        url: de.icr.io
+        namespace: dgp-registry
+        imagename: postservice
+        buildnr: c76d5f032667da9326bb8df7e96f06a4e3362044
+        dir: postservice
+
+```
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REGISTRY_URL=$1
+REGISTRY_NAMESPACE=$2
+IMAGE_NAME=$3
+BUILD_NUMBER=$4
+DIR=$5
+
+echo -e "Variables de entorno de compilación:"
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "IMAGE_NAME=${IMAGE_NAME}"
+echo "BUILD_NUMBER=${BUILD_NUMBER}"
+
+# Obtenga más información sobre las variables de entorno disponibles en:
+# https://cloud.ibm.com/docs/services/ContinuousDelivery?topic=ContinuousDelivery-deliverypipeline_environment#deliverypipeline_environment
+
+# Para revisar o cambiar las opciones de compilación utilice:
+# ibmcloud cr build --help
+
+cd $DIR
+
+echo -e "Comprobando el Dockerfile en la raíz de repositorio"
+if [ -f Dockerfile ]; then 
+   echo "Se ha encontrado el Dockerfile"
+else
+    echo "No se ha encontrado el Dockerfile"
+    exit 1
+fi
+
+echo -e "Compilando la imagen del contenedor"
+set -x
+ibmcloud cr build -t $REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$BUILD_NUMBER .
+set +x
